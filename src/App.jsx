34453eb9af04a57d5f5e24889bfa4fcdd757028b
Nitin-Shivhare <nitin.alt99@gmail.com>
2026-01-10 02:18:32 +0530

    added BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import "./App.css"
+import AppLayout from "./component/AppLayout"
+import HomePage from "./page/HomePage"
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <AppLayout />,
+		children: [
+			{
+				index: true,
+				element: <HomePage />,
+			},
+			// {
+			// 	path: "signup",
+			// 	element: <UserAuthFormpage type="Sign-Up" />,
+			// },
+		],
+	},
+])
 
 function App() {
 	return (
-		<>
-			{" "}
-			world
-			<button>Click me</button>
-			<div>Hello world</div>
-		</>
+		<div className="App">
+			<RouterProvider router={router} />
+		</div>
 	)
 }
 
