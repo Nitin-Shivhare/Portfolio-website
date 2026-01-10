bf0b8e6e3fef02e5c31b1db6bd4c830c2d235f16
Nitin-Shivhare <nitin.alt99@gmail.com>
2026-01-11 00:38:04 +0530

    Added ShowcaseApp

---
+++ src/component/ShowcaseApp.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react"
+import "../styles/ShowcaseApp.css"
+import RetroWindow from "./RetroWindow"
+import ShowcaseContentHome from "./ShowcaseContentHome"
+import ShowcaseContentAbout from "./ShowcaseContentAbout"
+import ShowcaseContentExperience from "./ShowcaseContentExperience"
+import ShowcaseContentContact from "./ShowcaseContentContact"
+import ShowcaseContentProjects from "./ShowcaseContentProjects"
+
+function ShowcaseApp({ setIsShowcase }) {
+	const [showcaseAppState, setShowcaseAppState] = useState("about")
+
+	return (
+		<div>
+			<RetroWindow
+				setIsShowcase={setIsShowcase}
+				title={"My Showcase - Nitin Shivhare"}
+			>
+				{/* HOME */}
+				{showcaseAppState == "home" && (
+					<ShowcaseContentHome setShowcaseAppState={setShowcaseAppState} />
+				)}
+
+				{/* ABOUT */}
+				{showcaseAppState == "about" && (
+					<ShowcaseContentAbout setShowcaseAppState={setShowcaseAppState} />
+				)}
+
+				{/* PROJECTS */}
+				{showcaseAppState == "projects" && (
+					<ShowcaseContentProjects setShowcaseAppState={setShowcaseAppState} />
+				)}
+
+				{/* experience */}
+				{showcaseAppState == "experience" && (
+					<ShowcaseContentExperience
+						setShowcaseAppState={setShowcaseAppState}
+					/>
+				)}
+
+				{/* CONTACT */}
+				{showcaseAppState == "contact" && (
+					<ShowcaseContentContact setShowcaseAppState={setShowcaseAppState} />
+				)}
+			</RetroWindow>
+
+			{/* <div className="status-bar">
+				<span>© 2026 Nitin Shivhare</span>
+			</div> */}
+		</div>
+	)
+}
+
+export default ShowcaseApp
