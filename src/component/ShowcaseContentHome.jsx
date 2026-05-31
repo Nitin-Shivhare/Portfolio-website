cf946884c7dfe5e3d81496e959a6ffbb199f17ce
Nitin-Shivhare <nitin.alt99@gmail.com>
2026-05-31 11:26:43 +0530

    Add: skills portion in ShowcaseContentHome Page

--- src/component/ShowcaseContentHome.jsx
+++ src/component/ShowcaseContentHome.jsx
@@ -12,6 +12,10 @@ function ShowcaseContentHome({ setShowcaseAppState }) {
 			<div className="outer-box">
 				<h1 className="center">Nitin Shivhare</h1>
 				<h3 className="center">Software Engineer</h3>
+				<p className="center">
+					Full Stack Developer React • Next.js • TypeScript • Node.js •
+					PostgreSQL
+				</p>
 				<div className="third clickable">
 					<h4 onClick={handleShowcaseAppState}>ABOUT</h4>
 					<h4 onClick={handleShowcaseAppState}>PROJECTS</h4>
